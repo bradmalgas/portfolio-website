30b976e49401b1f2c1dd1e30f20dbb7626643e72
Brad Malgas <bradstermalgas@gmail.com>
2025-01-28 20:10:21 +0200

    Updated stock picture

--- portfolio/app/projects/page.tsx
+++ portfolio/app/projects/page.tsx
@@ -73,7 +73,7 @@ export default function Projects() {
           />
           <ProjectCard
             title="Project 02: Blog Website"
-            image="/git-logo.png"
+            image="/office-background.jpg"
             description="I'm a paragraph. Click here to add your own text and edit me. It's easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I'm a great place for you to tell a story and let your users know a little more about you."
           />
           <ProjectCard
@@ -83,7 +83,7 @@ export default function Projects() {
           />
           <ProjectCard
             title="Project 02: Blog Website"
-            image="/git-logo.png"
+            image="/office-background.jpg"
             description="I'm a paragraph. Click here to add your own text and edit me. It's easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I'm a great place for you to tell a story and let your users know a little more about you."
           />
         </div>
