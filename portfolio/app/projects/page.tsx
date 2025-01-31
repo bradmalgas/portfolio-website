c305f798462d9a7b47de669a607404b7f7c9eb62
Brad Malgas <bradstermalgas@gmail.com>
2025-01-31 17:18:56 +0200

    Updated project links

--- portfolio/app/projects/page.tsx
+++ portfolio/app/projects/page.tsx
@@ -70,11 +70,19 @@ export default function Projects() {
             title="Project 01: BudgetBuddy"
             image="/BudgetBuddy-Screenshot.png"
             description="A real-time transaction tracker that runs code on my bank card to log every purchase. It triggers an Azure Function, formats the data, and writes it to Google Sheets—all in under 3 seconds. Built for personal finance tracking, this project showcases cloud automation and API integrations."
+            githubLink={"https://github.com/bradmalgas/programmable-banking"}
+            blogLink={
+              "https://blog.bradmalgas.com/post/Azure/701cb780-9f65-4299-8bc4-7aaf6adb67bc"
+            }
           />
           <ProjectCard
             title="Project 02: AzureBlogify"
             image="/OG-Brad Malgas.png"
             description="A fully functional, cost-optimized blog running on Azure for just $1. Hosted on Azure Static Web Apps, powered by Azure Functions, and backed by Cosmos DB. Designed with seamless CI/CD, caching, and scalability, it demonstrates cloud-native development at minimal cost."
+            githubLink={"https://github.com/bradmalgas/AzureBlogify"}
+            blogLink={
+              "https://blog.bradmalgas.com/post/Azure/f38daba8-de4b-4d56-80a5-c9c884efe25e"
+            }
           />
         </div>
       </section>
