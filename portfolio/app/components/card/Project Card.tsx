a61c6dec6c1fe307abdfe08a0a948b85dbdabbcb
Brad Malgas <bradstermalgas@gmail.com>
2025-01-31 13:56:09 +0200

    Limit size of images on projects page

--- portfolio/app/components/card/Project Card.tsx
+++ portfolio/app/components/card/Project Card.tsx
@@ -12,12 +12,18 @@ export default function ProjectCard({
   description,
 }: ProjectCardProps) {
   return (
-    <div className="bg-gray-600 py-5 px-4 flex-col rounded-xl outline outline-[6px] outline-gray-300 text-white drop-shadow-sm max-w-[400px] xl:max-w-[800px]">
+    <div className="bg-gray-600 py-5 px-4 flex-col rounded-xl outline outline-[6px] outline-gray-300 text-white drop-shadow-sm max-w-[400px] xl:max-w-[600px]">
       <p className="font-bold text-3xl tracking-widest">{title}</p>
-      <div className="relative w-full h-[180px] mx-auto my-8 flex items-center">
-        <Image src={image} alt={title} fill style={{ objectFit: "contain" }} />
+      <div className="relative w-full h-[300px] mx-auto my-8 px-8 flex items-center">
+        <Image
+          className="rounded-md"
+          src={image}
+          alt={title}
+          fill
+          style={{ objectFit: "cover" }}
+        />
       </div>
-      <p className="text-[13px] leading-tight px-2">{description}</p>
+      <p className="text-sm leading-tight px-2">{description}</p>
     </div>
   );
 }
