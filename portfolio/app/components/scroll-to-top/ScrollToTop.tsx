b892996adeb3bdaacf3a165acd23ae61ecd40876
Brad Malgas <bradstermalgas@gmail.com>
2025-02-01 15:35:34 +0200

    Added scroll to top component

---
+++ portfolio/app/components/scroll-to-top/ScrollToTop.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useState, useEffect } from "react";
+import ArrowUp from "../icons/ArrowUp";
+
+export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 200) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <button
+      onClick={scrollToTop}
+      className={`fixed bottom-10 right-5 rounded-full bg-black shadow-lg transition-opacity duration-300 ${
+        isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+      }`}
+    >
+      <ArrowUp className="h-10 w-10" fillColour="#ffffff" />
+    </button>
+  );
+}
